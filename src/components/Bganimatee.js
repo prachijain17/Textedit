ce07537f0404c5c97464b3178b1fd6167a60e2c6
prachi <prachi9873@gmail.com>
2023-10-01 13:54:52 +0530

    initial commit

---
+++ src/components/Bganimatee.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../App.css';
+const Bganimatee = () => {
+  return (
+    <div>
+    <ul className="background">
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+</ul>
+    </div>
+  )
+}
+
+export default Bganimatee
